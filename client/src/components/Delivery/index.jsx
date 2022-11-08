38acfb4ced690b199587476adbdf66de7e0c6239
Rohit-parjapat <rparjapati7377@gmail.com>
2022-11-08 12:32:45 +0530

    changes in client and sh file

--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -22,7 +22,7 @@ const Delivery = () => {
     <>
       <DeliveryCarousel />
       <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
-        Delivery Restaurants in NCR (Delhi)
+        Delivery Restaurants in Gurugram
       </h1>
       <div className="grid gap-0 md:gap-2 md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
         {restaurantList.map((restaurant) => (
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh "ubuntu@3.109.207.92" "cd app/gitlab-zomato-clone-project && sudo docker-compose stop && git pull && sudo docker-compose up -d --build && exit"
+ssh "ubuntu@15.207.111.146" "cd app/gitlab-zomato-clone-project && sudo docker-compose stop && git pull && sudo docker-compose up -d --build && exit"
