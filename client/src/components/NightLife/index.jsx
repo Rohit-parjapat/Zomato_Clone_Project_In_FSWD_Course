64eec4bf68014a276a73807b3d002998ae8416eb
Rohit-parjapat <rparjapati7377@gmail.com>
2022-11-09 21:19:49 +0530

    Added README.md file and updated client component

--- client/src/components/NightLife/index.jsx
+++ client/src/components/NightLife/index.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RestaurantCard from "../RestaurantCard";
 
 import NightLifeCarousel from "./NightLifeCarousel";
 
+// redux
+import { useSelector } from "react-redux";
+
 const NightLife = () => {
-    return (
-        <div className="mb-10">
-            <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
-                Nightlife Restaurant in Delhi NCR
-            </h1>
-            <NightLifeCarousel />
-        </div>
-    );
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  const reduxState = useSelector(
+    (globalState) => globalState.restaurant.restaurants
+  );
+
+  useEffect(() => {
+    reduxState && setRestaurantList(reduxState);
+  }, [reduxState]);
+  return (
+    <div className="mb-10">
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Nightlife Restaurant in Delhi NCR
+      </h1>
+      <NightLifeCarousel />
+      <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
+        Nightlife Restaurants in Gurugram
+      </h1>
+      <div className="grid gap-0 md:gap-2 md:grid-cols-2 grid-cols-1 lg:grid-cols-3">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard {...restaurant} key={restaurant._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NightLife;
