0fd981d18d5d2615c60a4c259e7c0e7686393efd
Rohit-parjapat <rparjapati7377@gmail.com>
2022-11-14 18:22:03 +0530

    updated ip

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh "ubuntu@43.204.97.221" "cd app/gitlab-zomato-clone-project && sudo docker-compose stop && git pull && sudo docker-compose up -d --build && exit"
+ssh "ubuntu@13.233.193.212" "cd app/gitlab-zomato-clone-project && sudo docker-compose stop && git pull && sudo docker-compose up -d --build && exit"
